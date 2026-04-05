741f994a40c77c6a8d23efde69db96d1b858f9bc
gaoxuefeng <gxf470001@163.com>
2026-04-05 16:42:30 +0800

    fix: use relative path for AOXFoundationKit dependency

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "AOXUIKit", targets: ["AOXUIKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/GxFn/AOXFoundationKit.git", from: "0.1.0"),
+        .package(path: "../AOXFoundationKit"),
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.7.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.0.0"),
